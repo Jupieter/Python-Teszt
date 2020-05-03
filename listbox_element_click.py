99f5ba7aee5a2f877817ccf597579b7db7d81f98
Jupieter <peterjung777@gmail.com>
2020-05-03 23:17:44 +0200

    Scrollbar Today
    The X view missed

--- listbox_element_click.py
+++ listbox_element_click.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import *
 my_w = tk.Tk()
-my_w.geometry("500x500")  # Size of the window 
+my_w.geometry("300x300")  # Size of the window 
 
 def my_upd(my_widget):
     my_w = my_widget.widget
@@ -14,19 +14,19 @@ def my_updel():
     #l1.delete(2)  # Delete the 2nd position element     
 
 my_font=('times', 8, 'italic')   
-l1 = tk.Listbox(my_w, height=6, width=15, bg='yellow', bd=5, cursor='boat',
+l1 = tk.Listbox(my_w, height=6, width=40, bg='yellow', bd=5, cursor='boat',
                 font=my_font, fg='green',
                 highlightcolor='blue', highlightthickness=15,
                 selectbackground='orange', selectforeground='cyan',
                 selectmode='multiple'
                 #selectmode: It can be SINGLE, BROWSE, MULTIPLE, EXTENDED
                 )
-l1.pack() 
-#sb = Scrollbar(my_w)
-#sb.pack(side=left, fill="Y")
+l1.pack(side=LEFT, fill=BOTH) 
+sb = Scrollbar(l1)
+sb.pack(side=RIGHT, fill=Y)
 
-#l1.config(yscrollcommand=scrollbar.set)
-#sb.config(command=listbox.yview)
+l1.config(yscrollcommand=sb.set)
+sb.config(command=listbox.yview)
 
 my_list=[('PHP',1),('Python',2),('MySQL',3),'JavaScript','JQuery','PHP','Python','MySQL']
 for element in my_list:
@@ -35,7 +35,7 @@ for element in my_list:
 
 #b1 = tk.Button(my_w, text="Delete", command=lambda l1=l1: l1.delete(ANCHOR))  #short method
 b1 = tk.Button(my_w, text="Delete", command=lambda: my_updel())
-b1.pack(side=TOP)
+b1.pack(side=BOTTOM)
 
 l1.bind('<<ListboxSelect>>', my_upd)
     
