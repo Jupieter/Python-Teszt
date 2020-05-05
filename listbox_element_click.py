c3f89ecd49ec2070116bf8c5dd8e05a1b17589bb
Jupieter <peterjung777@gmail.com>
2020-05-05 18:55:32 +0200

    Update listbox_element_click.py

--- listbox_element_click.py
+++ listbox_element_click.py
@@ -14,19 +14,19 @@ def my_updel():
     #l1.delete(2)  # Delete the 2nd position element     
 
 my_font=('times', 8, 'italic')   
-l1 = tk.Listbox(my_w, height=6, width=15, bg='yellow', bd=5, cursor='boat',
+l1 = tk.Listbox(my_w, height=8, width=100, bg='yellow', bd=5, cursor='boat',
                 font=my_font, fg='green',
                 highlightcolor='blue', highlightthickness=15,
                 selectbackground='orange', selectforeground='cyan',
                 selectmode='multiple'
                 #selectmode: It can be SINGLE, BROWSE, MULTIPLE, EXTENDED
                 )
-l1.pack() 
-#sb = Scrollbar(my_w)
-#sb.pack(side=left, fill="Y")
-
-#l1.config(yscrollcommand=scrollbar.set)
-#sb.config(command=listbox.yview)
+l1.pack(side=TOP, anchor=CENTER, ipady=0, ipadx=40) 
+sb = Scrollbar(l1)
+sb.pack(side=RIGHT, fill=Y)
+l1.config(yscrollcommand=sb.set)
+sb.config(command=l1.yview)
+print(l1.pack_info () )
 
 my_list=[('PHP',1),('Python',2),('MySQL',3),'JavaScript','JQuery','PHP','Python','MySQL']
 for element in my_list:
